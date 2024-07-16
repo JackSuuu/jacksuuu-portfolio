05c7c846990685641b6aaa66b704ac050e7bbb19
Jack <jack@JACKs-MacBook-Pro.local>
2024-07-16 16:24:27 +0800

    modify the main page to display the heading Jack correctly

--- public/home_page.js
+++ public/home_page.js
@@ -1,17 +1,18 @@
 // Define your components here
 
 function App() {
+
     return (
-        <section>
+        <section className="mb-10">
             <div class="flex justify-between items-center p-7 text-white">
                 <button class="transition duration-300 ease-in-out hover:text-[#DBDBDB]">@ code by Jack</button> 
                 <ul class="flex justify-between item-center p-3 gap-x-3 border border-3.5 rounded-lg">
-                    <li><button class="transition duration-300 ease-in-out hover:text-[#DBDBDB]">Intro</button></li>
+                    <li><a href="#intro-page" class="transition duration-300 ease-in-out hover:text-[#DBDBDB]">Intro</a></li>
                     <li><button class="transition duration-300 ease-in-out hover:text-[#DBDBDB]">Work</button></li>
                     <li><button class="transition duration-300 ease-in-out hover:text-[#DBDBDB]">Contact</button></li>
                 </ul>
             </div>
-            <div class="p-10 text-white">
+            <div class="p-10 text-white flex flex-col items-center justify-center bg-background text-foreground">
                 <div class="relative">
                     <h2 class="absolute top-5 left-5 text-white">
                         Computer Science Student &
@@ -19,11 +20,13 @@ function App() {
                     <h2 class="absolute top-11 left-5 text-white">
                         Developer, Designer
                     </h2>
-                    <img src="assets/home_page_bg.gif" loop></img>
-                    <h1 class="text-8xl mt-2 absolute top-80 font-inter">
-                    JACK SU
+                    <img src="assets/home_page_bg.gif" loop class="max-h-[500px]"></img>
+                    <h1 className={`text-8xl mt-10 absolute left-[30px] bottom-[-40px] font-inter`}>
+                        JACK SU
                     </h1>
-                    <div class="text-sm text-[#DBDBDB] absolute mt-20 top-100 font-inter flex justify-between">
+                    <div class="flex justify-between gap-x-6 text-m text-[#DBDBDB] absolute mt-15 bottom-[-80px] font-inter @media (min-width: 800px){
+                        bottom-[-120px]
+                    }">
                         <span>Design-driven programming</span>
                         <span>Artificial Intelligence</span>
                         <span>Detail-oriented</span>
